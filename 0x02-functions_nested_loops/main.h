66662190cddf9ed8eb2e45df605bb96c340d6530
DellamwarEddy <rayeddy92@gmail.com>
2022-09-14 04:07:53 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,27 +4,27 @@
 
 
 
-Int _putchar(char);
+int _putchar(char);
 
 Void print_alphabet(void);
 
 Void print_alphabet_x10(void);
 
-Int _islower(int c);
+int _islower(int c);
 
-Int _isalpha(int c);
+int _isalpha(int c);
 
-Int print_sign(int n);
+int print_sign(int n);
 
-Int _abs(int);
+int _abs(int);
 
-Int print_last_digit(int);
+int print_last_digit(int);
 
 Void jack_bauer(void);
 
 Void times_table(void);
 
-Int add(int a, int b);
+int add(int a, int b);
 
 Void print_to_98(int n);
 
